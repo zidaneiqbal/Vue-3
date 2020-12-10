26cccd175ab2b260846dcec1b3ab4a7e38bcd4f1
Zidane <arjuna_wira_27rpl@student.smktelkom-mlg.sch.id>
2020-12-10 21:07:03 +0700

    Commit Malam

---
+++ src/components/First.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1> {{msg}} </h1>
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        color: white;
+        padding: 10px 5px;
+        background-color: grey;
+    }
+</style>
+
+<script>
+export default {
+    name: "First",
+    props: {
+        msg: String
+    }
+}
+</script>
